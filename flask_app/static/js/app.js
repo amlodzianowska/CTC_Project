1794a898d35511afd606a52b0f8dc16a1fb474da
amlodzianowska <an.mlodzianowska@gmail.com>
2022-02-14 18:02:50 -0600

    First commit

---
+++ flask_app/static/js/app.js
@@ -0,0 +1,10 @@
+function initMap() {
+    // var options = {
+    //     center: { lat: 41.931, lng: -87.647 },
+    //     zoom: 8,
+    // }
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 41.931, lng: -87.647 },
+        zoom: 8,
+    });
+}
